ea8917cc923562fb280993b2e4b9bec7b10148ee
Jorge Cellerino <jorge.X.Cellerino.-ND@email.disney.com>
2015-12-03 18:19:28 -0300

    PassingCars exercise added

---
+++ src/codility/lesson3/prefixSums/PassingCars.java
@@ -0,0 +1,39 @@
+package codility.lesson3.prefixSums;
+
+/**
+ * 100%
+ * @author Jorge
+ *
+ */
+public class PassingCars {
+
+    private static final int EAST = 0;
+    private static final int WEST = 1;
+    
+    public static void main(String[] args){
+        //int[] A = {0,1,0,1,1};
+        //int[] A = {1,0};
+        int[] A = {0,1};
+        System.out.println(new PassingCars().solution(A));
+    }
+
+    public int solution(int[] A) {
+        int goingEast = 0;
+        int passingCars = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == EAST) {
+                goingEast++;
+            } else if(A[i] == WEST){
+                passingCars = passingCars + goingEast;
+                if(passingCars > 1000000000){
+                    return -1;
+                }
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return passingCars;
+    }
+
+}
