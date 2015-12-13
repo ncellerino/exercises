88b0b606dce91289fa77fc3bdaec5fdfe79a051f
Jorge Cellerino <jorge.X.Cellerino.-ND@email.disney.com>
2015-12-13 13:55:08 -0300

    Distinct exercise added

---
+++ src/codility/lesson4/sorting/Distinct.java
@@ -0,0 +1,30 @@
+package codility.lesson4.sorting;
+
+import java.util.Arrays;
+
+public class Distinct {
+    
+    public static void main(String[] args){
+        int[] A = {2,1,1,2,3,1};
+        System.out.println(new Distinct().solution(A));
+    }
+    
+    public int solution(int[] A) {
+        if(A.length == 0){
+            return 0;
+        }
+        
+        Arrays.sort(A);
+        
+        int previous = A[0]; 
+        int counter = 1;
+        for(int i = 1; i < A.length; i++){
+            if(A[i] != previous){
+                counter++;
+                previous = A[i];
+            }
+        }
+        return counter;
+    }
+
+}
