463b495469daf92d5f630fc1598ea0b4fa43fa87
ncellerino <ncellerino@gmail.com>
2015-12-16 12:06:53 -0800

    lesson 7 exercises added

---
+++ src/codility/lesson7/maximumSliceProblem/MaxSliceSum.java
@@ -0,0 +1,25 @@
+package codility.lesson7.maximumSliceProblem;
+
+/**
+ * 100%
+ * @author jorge.cellerino
+ *
+ */
+public class MaxSliceSum {
+    
+    public static void main(String[] args){
+       int[] A = {3,2,-6,4,0};
+       // int[] A = {-4,-2,-6,-1};
+        System.out.println(new MaxSliceSum().solution(A));
+    }
+
+    public int solution(int[] A) {
+        int maxEnding = A[0];
+        int goldenMax = A[0];
+        for (int i = 1; i < A.length; i++) {
+            maxEnding = Math.max(A[i], A[i] + maxEnding);
+            goldenMax = Math.max(goldenMax, maxEnding);
+        }
+        return goldenMax;
+    }
+}
