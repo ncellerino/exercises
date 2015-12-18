3d36f343e2b9c9509e17ae0a22e010a65bb0cc16
ncellerino <ncellerino@gmail.com>
2015-12-18 07:12:21 -0800

    exercise added

---
+++ src/codility/lesson8/primeAndCompositeNumbers/MinPerimeterRectangle.java
@@ -0,0 +1,25 @@
+package codility.lesson8.primeAndCompositeNumbers;
+
+public class MinPerimeterRectangle {
+
+    public static void main(String args[]) {
+        int N = 36;
+        System.out.println(new MinPerimeterRectangle().solution(N));
+    }
+
+    public int solution(int N) {
+
+        int i = 2;
+        int minPerimeter = 2 * (1 + N);
+        int currentPerimeter = 0;
+        while (i * i <= N) {
+            if (N % i == 0) {
+                int B = N / i;
+                currentPerimeter = 2 * (i + B);
+                minPerimeter = Math.min(currentPerimeter, minPerimeter);
+            }
+            i++;
+        }
+        return minPerimeter;
+    }
+}
